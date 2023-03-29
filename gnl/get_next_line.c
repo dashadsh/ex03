85f137166387329234b2795826e00f9decc721bd
Daria Goremykina <d.goremykina@gmail.com>
2023-03-29 23:37:55 +0200

    comm

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,7 +1,15 @@
+// #ifndef GET_NEXT_LINE_H
+// # define GET_NEXT_LINE_H
+// #endif
+
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 1024
+
 // #include <unistd.h> // For read
 // #include <stdlib.h> // For malloc && free
 
-// // #define BUFFER_SIZE 1024
+// char    *get_next_line(int fd);
+// #endif
 
 #include "get_next_line.h"
 
@@ -10,24 +18,24 @@ char	*get_next_line(int fd)
 	int		index;
 	int		byte;
 	char	*buffer;
-	char	character;
+	char	c;
 
 	if ((fd < 0) || (BUFFER_SIZE <= 0))
 		return (NULL);
 	index = 0;
 
 	buffer = malloc(sizeof(char) * 1000);
-	byte = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1);
+	byte = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1);
 	// buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	// byte = read(fd, &character, 1);
 	if (byte <= 0)// nothing was made AT ALL, extreme chedck
 		return (free(buffer), NULL);
 	while (byte > 0)
 	{
-		buffer[index++] = character;
-		if (character == '\n')
+		buffer[index++] = c;
+		if (c == '\n')
 			break ;
-		byte = read(fd, &character, 1);
+		byte = read(fd, &c, 1);
 	}
 	//if ((byte <= 0) && (index == 0)) // nothing was made AT ALL, extreme chedck
 	//	return (free(buffer), NULL);
--- printf/printf.c
+++ printf/printf.c
@@ -22,8 +22,8 @@ void hexy(long long int nb, int base, int *strlen)
 		*strlen += write(1, "-",1); // "" cus pointer
 		nb *= -1;
 	}
-	if (nb >= base) //> AND =
-		hexy(nb/base, base, strlen); // sic- just strlen
+	if (nb >= base) // > AND = !!!
+		hexy(nb/base, base, strlen); // SIC - just strlen
 	*strlen += write(1, &h[nb%base], 1);
 }
 int ft_printf(const char *format, ...)
@@ -40,17 +40,18 @@ int ft_printf(const char *format, ...)
 			if (*format == 's')
 				stringhy(va_arg(ap, char *), &strlen);
 			else if (*format == 'd')
-				hexy((long long int)va_arg(ap, int), 10, &strlen); //not an array, not *
+				hexy((long long int)va_arg(ap, int), 10, &strlen); // not an array, not *
 			else if (*format == 'x')
 				hexy((long long int)va_arg(ap, unsigned int), 16, &strlen);
 		}
 		else
-			strlen += write(1, format, 1); // not a ptr HERE
+			strlen += write(1, format, 1); // not a & HERE
 		format++;
 	}
 	va_end(ap);
 	return(strlen);
 }
+
 // int main()
 // {
 // 	ft_printf("PRINTING: %s\n", "string");
